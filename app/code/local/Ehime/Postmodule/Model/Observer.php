b504f0052c8ae811f65c0fc91f7e98082778cbfe
ehime <dodomeki@gmail.com>
2015-03-26 15:11:07 -0700

    Notating

--- app/code/local/Ehime/Postmodule/Model/Observer.php
+++ app/code/local/Ehime/Postmodule/Model/Observer.php
@@ -3,6 +3,12 @@ Class Ehime_Postmodule_Model_Observer
 {
     public function ping(Varien_Event_Observer $observer)
     {
+//        // im not sure how to fire this prior to the events, but maybe we can detect and discard?
+//        $event = $observer->getEvent();
+//        if (! isset($event)) {
+//            return $this;
+//        }
+
         $data = $observer->getData('order_ids');
         $url  = 'http://foo.com'; // needs to be fetched...
 
