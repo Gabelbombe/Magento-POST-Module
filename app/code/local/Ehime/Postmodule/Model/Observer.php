36d9d70c0c377af8ec80927c89084efd6a6a5071
ehime <dodomeki@gmail.com>
2015-03-26 15:02:17 -0700

    Observer upo

---
+++ app/code/local/Ehime/Postmodule/Model/Observer.php
@@ -0,0 +1,33 @@
+<?php
+Class Ehime_Postmodule_Model_Observer
+{
+    public function ping(Varien_Event_Observer $observer)
+    {
+        $data = $observer->getData('order_ids');
+        $url  = 'http://foo.com'; // needs to be fetched...
+
+        try
+        {
+            $client = New Zend_Http_Client($url);
+
+            $client->setRawData(json_encode($data), 'application/json')->request('POST');
+
+            var_dump($client->request()->getBody());
+
+            Mage::log('Order has been sent to foo.com');
+
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+
+        return $this;
+    }
+
+    /**
+     * SHTF and needs to be cron'd i guess
+     */
+    private function pong()
+    {
+        // .....
+    }
+}
